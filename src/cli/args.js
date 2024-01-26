aed34abb4686d3113521123aaeddf5ccf02d8328
Lyanna Butolina <lyana@MacBook-Pro-Lyanna.local>
2024-01-26 22:21:37 +0500

    feat: implement parseArgs, parseEnv functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const result = args.reduce((prev, curr, index) => {
+    const delimiter = index === args.length - 1 ? '' : ', ';
+
+    if (index % 2) {
+      return prev + curr + delimiter;
+    } else {
+      const propName = curr.slice(2);
+      return prev + propName + ' is ';
+    }
+
+  }, '')
+
+  console.log(result);
 };
 
 parseArgs();
