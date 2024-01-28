dbe8a05b25deecac4ed2037d27adf2beb3bcfe91
Lyanna Butolina <lyana@MacBook-Pro-Lyanna.local>
2024-01-28 13:03:56 +0500

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import {createHash} from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'stream';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const folderPath = 'src/hash/files';
+  const fileToRead = 'fileToCalculateHashFor.txt';
+
+  const hash = createHash('sha256').setEncoding('hex');
+  const fileStream = createReadStream(path.join(folderPath, fileToRead));
+
+  pipeline(fileStream, hash, (err) => {
+    if (err) {
+      console.error('Error:', err);
+    } else {
+      console.log(hash.read());
+    }
+  })
 };
 
 await calculateHash();
