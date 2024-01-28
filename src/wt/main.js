97a0711aa224db2340677a3881f6b487b741557f
Lyanna Butolina <lyana@MacBook-Pro-Lyanna.local>
2024-01-28 20:53:40 +0500

    feat: implement worker thread functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,50 @@
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { cpus } from 'os';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const availableCpus = cpus();
+const resultArray = [];
+let counter = 0;
+
+const isLastIteration = () => {
+  if (counter === availableCpus.length) {
+    const sortedArray = resultArray.sort((a, b) => a.data - b.data);
+    console.log(sortedArray);
+  }
+}
+
 const performCalculations = async () => {
-    // Write your code here
+  availableCpus.forEach((cpu, index) => {
+    const fibInput = index + 10;
+
+    const worker = new Worker(join(__dirname, 'worker.js'), { workerData: fibInput });
+
+    worker.on('message', (msg) => {
+      resultArray.push({
+        status: 'resolved',
+        data: msg,
+      });
+
+      counter++;
+
+      isLastIteration();
+    });
+
+    worker.on('error', () => {
+      resultArray.push({
+        status: 'error',
+        data: null,
+      });
+
+      counter++;
+
+      isLastIteration();
+    });
+
+  });
 };
 
 await performCalculations();
