0a6344659580ff94a7fa9205be996bd7e88564e1
Lyanna Butolina <lyana@MacBook-Pro-Lyanna.local>
2024-01-28 14:12:19 +0500

    feat: implement stream write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+
 const write = async () => {
-    // Write your code here 
+  const fileToWrite = 'files/fileToWrite.txt';
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  const rs = createWriteStream(join(__dirname, fileToWrite));
+
+  pipeline(process.stdin, rs, (err) => {
+    if (err) {
+      console.error('Error:', err);
+    }
+  })
 };
 
 await write();
